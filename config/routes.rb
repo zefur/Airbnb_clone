962b71d4f55162cec35c2312d6043658a022b582
charinrat bansong <charinrat_b@hotmail.com>
2020-11-23 17:29:02 +0800

    edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/skills', to: 'skills#index'
-  get '/skills/:id', to: 'skills#show'
-  get '/:tag_id/index', to: 'skills#index'
+  resources :skills, only: [ :home, :index, :show]
+
+  end
+
 end
