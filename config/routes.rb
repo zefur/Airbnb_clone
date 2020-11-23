8e2e40b824549dd34f84c463c30609568b087043
charinrat bansong <charinrat_b@hotmail.com>
2020-11-23 17:01:10 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/skills', to: 'skills#index'
+  get '/skills/:id', to: 'skills#show'
+  get '/:tag_id/index', to: 'skills#index'
 end
