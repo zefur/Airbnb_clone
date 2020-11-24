b9232cd9a91e4f285b9a9d9984d597007a3adeef
charinrat bansong <charinrat_b@hotmail.com>
2020-11-24 10:54:14 +0800

    add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(params[:user])
+    @user.save
+    redirect_to user_path(@user)
   end
 
+  private
 
+    def user_params
+      params.require(:user).permit(:username, :email, :name, :contact, :location, :reviews, :avatar)
+    end
 end
