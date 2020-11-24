4eef4e4e00707a6da635174610c9d724fd0d467e
James Hall <jdhall@live.co.uk>
2020-11-24 02:54:39 +0000

    added the model relations, addedbookings routes, updated the controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,18 @@
 class BookingsController < ApplicationController
 
   def new
+    @booking = Booking.new
   end
 
   def create
-    
+    @booking = Booking.new(bookings_params)
+
+  end
+
+  private 
+  
+  def set_default
+    params.require(:booking).permit(:date,:skill_id,:user_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :skills, only: [ :home, :index, :show]
+resources :bookings, only: [:index, :new, :create, :delete]
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
