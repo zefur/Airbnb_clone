ad541cffa32d268e3ae110a91b3102727c84f907
charinrat bansong <charinrat_b@hotmail.com>
2020-11-24 11:23:38 +0800

    add def edit and def update, routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,20 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
-
   def show
     @user = User.find(params[:id])
   end
 
-  def create
-    @user = User.new(params[:user])
-    @user.save
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
     redirect_to user_path(@user)
   end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users ,only: [ :new, :create, :show]
+  resources :users ,only: [ :show, :edit, :update]
 end
