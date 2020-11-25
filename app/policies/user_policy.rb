483a88ab4df27d7a073f3f835e93480d2f921916
charinrat bansong <charinrat_b@hotmail.com>
2020-11-25 11:28:28 +0800

    modified user controller

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -9,8 +9,17 @@ class UserPolicy < ApplicationPolicy
     true
   end
 
-  def update?
-    recode.user == user
+  def show?
+    true
   end
 
+  def update?
+    user_is_owner_or_admin?
+  end
+
+  private
+
+  def user_is_owner_or_admin?
+    user == record.user || user.admin
+  end
 end
