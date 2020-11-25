29340200b468cb7ba9c550bc105df184ca72b5f4
charinrat bansong <charinrat_b@hotmail.com>
2020-11-25 11:37:18 +0800

    user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
-
+  before_action :set_skill, only: [:show, :edit, :update]
+  def index
+    @users = policy_scope(User).order(create_at: :desc)
+  end
+  
   def show
     # @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
+    authorize @user
   end
 
   def update
@@ -17,6 +22,11 @@ class UsersController < ApplicationController
 
 
   private
+    
+    def set_user
+    @user = User.find(params[:id])
+    authorize @user
+    end
 
     def user_params
       params.require(:user).permit(:username, :email, :name, :contact, :location, :reviews, :avatar)
