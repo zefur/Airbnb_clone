4392627c50bfbdff5659828ddf8afce05ba6d6f2
nadia al-jasem <nadiajasem@gmail.com>
2020-11-26 17:44:32 +0700

    changed the style of the skills show page while also changing the users show page - so will create new branch now

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,5 @@
 class Skill < ApplicationRecord
-  validates :name, :description, :price, :skill_location, :skill_type, presence: true 
+  validates :name, :description, :price, :skill_location, :skill_type, presence: true
   validates :description, length: {minimum: 50}
   validates :skill_type, inclusion: {in: %w[Driving DIY Coding Chores Teaching Gardening Craftwork "Pet care" "Baby care" Art]}
   belongs_to :user
@@ -7,8 +7,8 @@ class Skill < ApplicationRecord
 
   has_many_attached :works
 
-  acts_as_taggable_on :skill_types
-  
+  #acts_as_taggable_on :skill_types
+
 
 
 end
