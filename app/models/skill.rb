f3c3a4b5626f4b634a47d63c61426a7f97985299
James Hall <jdhall@live.co.uk>
2020-11-26 00:24:34 +0000

    changed routes and added taggable in the model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,9 +1,10 @@
 class Skill < ApplicationRecord
   validates :name, :description, :price, :skill_location, :skill_type, presence: true 
   validates :description, length: {minimum: 50}
+  validates :skill_type, inclusion: {in: %w[Driving DIY Coding Chores Teaching Gardening Craftwork "Pet care" "Baby care" Art]}
   belongs_to :user
   has_many :bookings
-
+  act_as_taggable :skill_type
   
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-resources :skills, only: [ :home, :index, :show]
-resources :bookings, only: [:index, :new, :create, :delete]
-resources :users, only: [:show, :edit, :update]
- 
+  resources :skills, only: [ :home, :index, :show]
+  resources :bookings, only: [:index, :new, :create, :delete]
+  resources :users, only: [:show, :edit, :update]
 
+  get '/tagged' to: 'skills#tagged', as: :tagged
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
