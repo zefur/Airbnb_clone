a06a1d03e5ad665728da230e425c1fa5e6dba52e
charinrat bansong <charinrat_b@hotmail.com>
2020-11-26 13:47:58 +0800

    display map and marker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,11 +28,10 @@ import { initMapbox } from '../plugins/init_mapbox';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-// document.addEventListener('turbolinks:load', () => {
-//   // Call your functions here, e.g:
-//   // initSelect2();
-// });
-
 document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
   initMapbox();
-})
+  
+});
+
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,8 +1,14 @@
 import mapboxgl from 'mapbox-gl';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-
+  
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
@@ -15,6 +21,7 @@ const initMapbox = () => {
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
     });
+    fitMapToMarkers(map, markers);
   }
 };
 export { initMapbox };
