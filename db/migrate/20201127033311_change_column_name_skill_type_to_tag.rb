07b7e6d907f8d0da454eda29c0c4a562e769a34b
nadia al-jasem <nadiajasem@gmail.com>
2020-11-27 13:10:01 +0700

    adjusted the form in the skills new page

--- db/migrate/20201127033311_change_column_name_skill_type_to_tag.rb
+++ db/migrate/20201127033311_change_column_name_skill_type_to_tag.rb
@@ -1,6 +1,6 @@
 class ChangeColumnNameSkillTypeToTag < ActiveRecord::Migration[6.0]
   def change
-    rename_column :skills, :tag, :tag
+    rename_column :skills, :skill_type, :tag
     #Ex:- rename_column("admin_users", "pasword","hashed_pasword")
   end
 end
