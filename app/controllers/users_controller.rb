037d04e2c2e815ca62fa1b07298fd74c5e392479
James Hall <jdhall@live.co.uk>
2020-11-27 04:35:08 +0000

    fixed the render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    @skill = Skill.new
     @user.update(user_params)
     redirect_to user_path(@user)
   end
