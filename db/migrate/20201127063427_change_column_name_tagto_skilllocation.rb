eecd130a35af32c92adb48cd7cec97cf842f31b6
charinrat bansong <charinrat_b@hotmail.com>
2020-11-27 15:15:39 +0800

    fix db:migrate

--- db/migrate/20201127063427_change_column_name_tagto_skilllocation.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNameTagtoSkilllocation < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :skills, :tag, :skill_location
-  end
-end
--- db/migrate/20201127063746_change_column_name_skilltype_to_tag.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNameSkilltypeToTag < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :skills, :skill_type, :tag
-  end
-end
